9b0e0e6cb2901a21d888a1c2a35a6c7b2a03098a
Lazersmoke <lazersmoke@gmail.com>
2018-02-21 16:12:27 -0600

    Add logon mana gain timer

--- src/main/java/com/github/maxopoly/MemeMana/listener/LoginListener.java
+++ src/main/java/com/github/maxopoly/MemeMana/listener/LoginListener.java
@@ -4,12 +4,25 @@ import com.github.maxopoly.MemeMana.MemeManaPlugin;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.scheduler.BukkitTask;
+import org.bukkit.Bukkit;
+import java.util.Optional;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.UUID;
 
 public class LoginListener implements Listener {
 
+	private static final Map<UUID,BukkitTask> giveManaTasks = new HashMap<UUID,BukkitTask>();
 	@EventHandler
 	public void playerJoin(PlayerJoinEvent e) {
-		MemeManaPlugin.getInstance().getActivityManager().updatePlayer(e.getPlayer().getUniqueId());
+		giveManaTasks.put(e.getPlayer().getUniqueId(),Bukkit.getScheduler().runTaskLater(MemeManaPlugin.getInstance(),() -> MemeManaPlugin.getInstance().getActivityManager().updatePlayer(e.getPlayer().getUniqueId()),36000L)); // 30 minutes in ticks
+	}
+
+	@EventHandler
+	public void playerJoin(PlayerQuitEvent e) {
+		Optional.ofNullable(giveManaTasks.remove(e.getPlayer().getUniqueId())).ifPresent(BukkitTask::cancel);
 	}
 
 }
