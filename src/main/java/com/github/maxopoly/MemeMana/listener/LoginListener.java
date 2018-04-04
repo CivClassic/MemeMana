f17bb5dd3bdb2ec93429f7e7fcbcce6b0ca8608c
iykHvfIvfR <iykHvfIvfR@users.noreply.github.com>
2018-04-04 13:08:57 +0000

    Fix English in the /manaclaim message

--- src/main/java/com/github/maxopoly/MemeMana/listener/LoginListener.java
+++ src/main/java/com/github/maxopoly/MemeMana/listener/LoginListener.java
@@ -20,7 +20,7 @@ public class LoginListener implements Listener {
 	@EventHandler
 	public void playerJoin(PlayerJoinEvent e) {
 		manaLoginTimes.put(e.getPlayer().getUniqueId(),System.currentTimeMillis());
-		notifyTasks.put(e.getPlayer().getUniqueId(),Bukkit.getScheduler().runTaskLater(MemeManaPlugin.getInstance(),() -> com.civclassic.altmanager.AltManager.instance().getAlts(e.getPlayer().getUniqueId()).forEach(u -> Optional.ofNullable(Bukkit.getPlayer(u)).ifPresent(p -> p.sendMessage(ChatColor.GREEN + "You're mana is ready to claim. Use " + ChatColor.YELLOW + "/manaclaim" + ChatColor.GREEN + " to receive it"))),MemeManaPlugin.getInstance().getManaConfig().getManaWaitTime()));
+		notifyTasks.put(e.getPlayer().getUniqueId(),Bukkit.getScheduler().runTaskLater(MemeManaPlugin.getInstance(),() -> com.civclassic.altmanager.AltManager.instance().getAlts(e.getPlayer().getUniqueId()).forEach(u -> Optional.ofNullable(Bukkit.getPlayer(u)).ifPresent(p -> p.sendMessage(ChatColor.GREEN + "Your mana is ready to claim. Use " + ChatColor.YELLOW + "/manaclaim" + ChatColor.GREEN + " to receive it"))),MemeManaPlugin.getInstance().getManaConfig().getManaWaitTime()));
 	}
 
 	@EventHandler
